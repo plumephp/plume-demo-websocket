7ef78750e439d255b7e842042651a3147a582129
zhangbaitong <zhangbaitong@163.com>
2017-05-03 16:30:23 +0800

    fix plumephp websocket config file

--- config/dev.php
+++ config/dev.php
@@ -1,26 +1,29 @@
 <?php
 return array(
+    //WebSocket绑定、分组、在线等信息存储使用的Redis配置
 	'redis'=>array('host' => '127.0.0.1','port'=>6379),
 	'actions_close' => array(
 		'example_index' => 'Example\Index',
 	),
-	'server' => array(
-		'master' => array(
-			'host' => '127.0.0.1',
-			'port' => '9501',
-			),
-		'slaves' => array(
-				array(
-					'host' => 'localhost',
-					'port' => '9502',
-				),
-				// array(
-				// 	'host' => '127.0.0.1',
-				// 	'port' => '9503',
-				// ),
-			)
-		),
-	'config' => array(
+    //本机WebSocket服务配置
+    'server_config' => array(
+        'allow_ip' => '0.0.0.0',
+        'host' => '127.0.0.1',
+        'port' => '9501',
+    ),
+    //WebSocket集群其它节点配置
+    'cluster_nodes' => array(
+        array(
+            'host' => '127.0.0.1',
+            'port' => '9502'
+        ),
+    ),
+    'ws_list' => array(
+        'ws://127.0.0.1:9501',
+        'ws://localhost:9502'
+    ),
+    //Swoole服务配置
+	'swoole_config' => array(
 			'timeout' => 2.5,  //select and epoll_wait timeout.
 			'poll_thread_num' => 2, //reactor thread num
 			'worker_num' => 1,      //worker process num，设置启动的worker进程数量，测试时使用1个即可
@@ -31,7 +34,7 @@ return array(
 			// 进程数据包分配模式: 1平均分配，2按FD取摸固定分配，3抢占式分配，默认为取摸(dispatch=2),4按IP分配，5按UID分配(需要用户代码中调用$serv->bind_uid(将一个连接绑定1个uid)
 			'dispatch_mode'=> 2,
 			'daemonize' => false,	// 若设置为true，执行php server.php将转入后台作为守护进程运行
-			'log_file' => "/Users/zhangtao/Documents/plumephp2016/plume-demo-websocket/var/logs/app.log", // daemonize为true时，输出内容才会写入到该日志文件
+			'log_file' => "/var/log/plume_swoole.log", // daemonize为true时，输出内容才会写入到该日志文件
 			//'heartbeat_idle_time' => 20,        // 连接最大允许空闲的时间(秒)，与heartbeat_check_interval配合使用.当dispatch_mode=1/3时，底层会屏蔽onConnect/onClose事件
 			//'heartbeat_check_interval' => 10,   // 表示每隔多久轮循一次，单位为秒
 		)
